72f9b4ae360d485e33707f6580d96ca46c2f8dbe
Mohammad Al Kiswani <mohammadkiswany@yahoo.com>
2013-03-05 02:14:50 +0300

    checkin all files

---
+++ TwitterReader/Controllers/TwittesController.cs
@@ -0,0 +1,98 @@
+﻿using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwitterReader.Models;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Linq;
+using System.Configuration;
+
+namespace TwitterReader.Controllers
+{
+    public class TwittesController : Controller
+    {
+        //
+        // GET: /Twittes/
+        public ActionResult Index()
+        {
+            var model = GetUserTwittes(ConfigurationManager.AppSettings["ScreenName"].ToString());
+            SaveModelToDB(model);
+            return View(model);
+        }
+
+        public ActionResult StatisticsPage()
+        {
+            return View(GetModelFromDB());
+        }
+
+        private void SaveModelToDB(Twittes model)
+        {
+            var mongo = new Mongo();
+            //Mohammadk: 03/04/2013 : connect to localhost
+            mongo.Connect();
+
+            var db = mongo.GetDatabase("Twittes");
+            var collection = db.GetCollection<Twittes>(ConfigurationManager.AppSettings["ScreenName"].ToString());
+
+            collection.Remove(p => true);
+
+            collection.Save(model);
+        }
+
+        private Twittes GetModelFromDB()
+        {
+            var mongo = new Mongo();
+            //Mohammadk: 03/04/2013 : connect to localhost
+            mongo.Connect();
+
+            var db = mongo.GetDatabase("Twittes");
+            var collection = db.GetCollection<Twittes>(ConfigurationManager.AppSettings["ScreenName"].ToString());
+            var model = new Twittes();
+
+            model.Tweets = (collection.Linq().ToList<Twittes>()[0]).Tweets;
+
+            return model;
+        }
+
+        private Twittes GetUserTwittes(string screenName)
+        {
+            var model = new Twittes();
+
+            RestClient client = new RestClient("http://api.twitter.com/1");
+            JsonDeserializer jsonDeserializer = new JsonDeserializer();
+          
+            var request = new RestRequest(Method.GET);
+
+            request.Resource = "statuses/user_timeline.json";
+
+            request.Parameters.Add(new Parameter()
+            {
+                Name = "screen_name",
+                Value = screenName,
+                Type = ParameterType.GetOrPost
+            });
+
+            request.Parameters.Add(new Parameter()
+            {
+                Name = "include_entities",
+                Value = true,
+                Type = ParameterType.GetOrPost
+            });
+
+            var response = client.Execute(request);
+
+            model.Tweets =
+              jsonDeserializer.Deserialize<List<Tweet>>(response);
+
+           
+
+            return model;
+
+        }
+
+    }
+}
---
+++ TwitterReader/Models/Twittes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterReader.Models
+{
+    public class User
+    {
+        public string profile_image_url { get; set; }
+        public string friends_count { get; set; }
+        public string followers_count { get; set; }
+        public string screen_name { get; set; }
+        public string statuses_count { get; set; }
+    }
+
+    public class Tweet
+    {
+        public string created_at { get; set; }
+        public string text { get; set; }
+        public User user { get { return Twittes.UserProfile; } set { Twittes.UserProfile = value; } }
+  
+    }
+
+    public class Twittes
+    {
+        public List<Tweet> Tweets { get; set; }
+        public static User UserProfile { get; set; }
+    }
+}
